08f98f491647908346e24a607343b4638dbefa82
Elianehbmna <elianehabimana3@gmail.com>
2019-07-18 17:48:11 +0200

    adjusting my statement flow

--- script.js
+++ script.js
@@ -22,7 +22,27 @@ console.log(gender);
  var cc = parseFloat(a);
  var day =   parseFloat(( ( (cc/4) -2*cc-1) + ((5*year/4) ) + ((26*(month+1)/10)) + dayi ) % 7);
   day = day.toFixed(0);
- if(day == 0 && gender == 'male'){
+  if(day == 0 && gender == 'female') {
+    document.getElementById("AkhanName").innerHTML ="Akosua";
+  }
+  else if(day == 1 && gender == 'female') {
+    document.getElementById("AkhanName").innerHTML ="Adwoa";
+  }
+  else if(day == 2 && gender == 'female') {
+  document.getElementById("AkhanName").innerHTML ="Abenaa";
+  }
+  else if(day == 3 && gender == 'female') {
+    document.getElementById("AkhanName").innerHTML ="Akua";
+  }
+  else if(day == 4 && gender == 'female') {
+    document.getElementById("AkhanName").innerHTML ="yaa";
+  }
+  else if(day == 5 && gender == 'female') {
+    document.getElementById("AkhanName").innerHTML ="Afua";
+  }
+  else if(day == 6 && gender == 'female') {
+    document.getElementById("AkhanName").innerHTML ="Ama";
+  }else if(day == 0 && gender == 'male'){
    document.getElementById("AkhanName").innerHTML ="kwasi";
  } 
  else if(day == 4 && gender == 'female') {
@@ -50,31 +70,8 @@ console.log(gender);
   else if(day == 6 && gender == 'male') {
  document.getElementById("AkhanName").innerHTML ="kwame";
   }
- else if(day == 0 && gender == 'female') {
- document.getElementById("AkhanName").innerHTML ="Akosua";
-  }
-  else if(day == 1 && gender == 'female') {
-  document.getElementById("AkhanName").innerHTML ="Adwoa";
 
-  }
- else if(day == 2 && gender == 'female') {
-  document.getElementById("AkhanName").value ="Abeena";
-
-}
-  else if(day == 3 && gender == 'female') {
-    document.getElementById("AkhanName").value ="Akua";
-
-  }
-  else if(day == 4 && gender == 'female') {
-    document.getElementById("AkhanName").value ="yaa";
-
-  }
-  else if(day == 5 && gender == 'female') {
-   document.getElementById("AkhanName").value ="Afua";
- }
-  else if(day == 6 && gender == 'female') {
-   document.getElementById("AkhanName").value ="Ama";
- } 
+  
  }
 
 
